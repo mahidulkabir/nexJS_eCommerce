06cb4275d3038652560da83f696f1c11e43ba2b2
mahidulkabir <mahidulkabir8@gmail.com>
2025-12-11 10:40:52 +0600

    first commit

---
+++ app/page.jsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className='text-3xl font-bold '>I am mahidul Kabir</h1>
+        <Button className='mx-4'>Click Me</Button>
+    </div>
+  )
+}
+
+export default page

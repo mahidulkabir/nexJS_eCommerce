1e78f05d5545e8ecbe788f97edb9079d12c9aa78
mahidulkabir <mahidulkabir8@gmail.com>
2025-12-12 16:19:41 +0600

    registration form done

---
+++ app/(root)/auth/login/page.jsx
@@ -0,0 +1,98 @@
+"use client";
+import { Card, CardContent } from "@/components/ui/card";
+import React from "react";
+import Logo from "@/public/assets/images/logo-black.png";
+import Image from "next/image";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { StrongAuthSchema } from "@/lib/zodSchema";
+import { useForm } from "react-hook-form";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+const LoginPage = () => {
+  const formSchema = StrongAuthSchema.pick({
+    email: true,
+    password: true,
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleLoginSubmit = async (value) => {};
+  return (
+    <Card className="w-[450px]">
+      <CardContent>
+        <div className="flex justify-center">
+          <Image
+            src={Logo.src}
+            width={Logo.width}
+            height={Logo.height}
+            alt="log in logo"
+            className="max-w-[150px]"
+          />
+        </div>
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Login Into Account</h1>
+          <p>Login into your account by filling out the form below</p>
+        </div>
+        <div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(handleLoginSubmit)}>
+              <div className="my-3">
+                <FormField
+                  control={form.control}
+                  name="email"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Email</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="email"
+                          placeholder="example@gmail.com"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="mb-3">
+                <FormField
+                  control={form.control}
+                  name="password"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Password</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="password"
+                          placeholder="******"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+            </form>
+          </Form>
+        </div>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default LoginPage;

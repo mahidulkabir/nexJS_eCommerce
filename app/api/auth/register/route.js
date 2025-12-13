7f2e849e0fd30d120716f45277a8c66a94ed765a
mahidulkabir <mahidulkabir8@gmail.com>
2025-12-13 12:45:32 +0600

    added some of the auth route and process

--- app/api/auth/register/route.js
+++ app/api/auth/register/route.js
@@ -1,9 +1,41 @@
 import { connectDB } from "@/lib/databaseConnection";
+import { response } from "@/lib/helperFunction";
+import { StrongAuthSchema } from "@/lib/zodSchema";
+import UserModel from "@/models/User.model";
+import { SignJWT } from "jose";
 
-export async function POST (request){
-    try{
-        await connectDB()
-    }catch (error){
-        
+
+export async function POST(request) {
+  try {
+    await connectDB();
+    // validation schema 
+    const validationSchema = StrongAuthSchema.pick({
+        name:true, email:true, password:true
+    })
+    const payload = await request.json()
+    const validatedData = validationSchema.safeParse(payload)
+    if(!validatedData.success){
+        return response(false,401,'invalid or missing input field.',
+            validatedData.error
+        )
     }
+    const {name,email,password} = validatedData.data
+    // check already registerd user 
+    const checkUser = await UserModel.exists({email})
+    if(checkUser){
+        return response(true,409,'User already registered.',validatedData.error)
+    }
+    // new registration 
+    const NewRegistration = new UserModel({
+        name, email, password
+    })
+    await NewRegistration.save()
+
+    const secret = new TextEncoder().encode(process.env.SECRET_KEY)
+    const token = await new SignJWT({userId: NewRegistration._id})
+    .setIssuedAt()
+    .setExpirationTime('1hr')
+    .setProtectedHeader({alg: 'HS256'})
+    .sign(secret)
+  } catch (error) {}
 }

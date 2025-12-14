1abf214ef01c8e5709ee8f7d6dde8c8d7db74fcb
mahidulkabir <mahidulkabir8@gmail.com>
2025-12-14 23:13:07 +0600

    added otp generation code

---
+++ app/api/auth/login/route.js
@@ -0,0 +1,94 @@
+import { emailVerificationLink } from "@/email/emailVerificationLink";
+import { otpEmail } from "@/email/otpEmail";
+import { connectDB } from "@/lib/databaseConnection";
+import { catchError, generateOTP, response } from "@/lib/helperFunction";
+import { sendMail } from "@/lib/sendMail";
+import { StrongAuthSchema } from "@/lib/zodSchema";
+import OTPModel from "@/models/Otp.model";
+import UserModel from "@/models/User.model";
+import { SignJWT } from "jose";
+import { z } from "zod";
+
+export async function POST(request) {
+  try {
+    await connectDB();
+    const payload = await request.json();
+    const validationSchema = StrongAuthSchema.pick({
+      email: true,
+    }).extend({
+      password: z.string(),
+    });
+    const validatedData = validationSchema.safeParse(payload);
+    if (!validatedData.success) {
+      return response(
+        false,
+        401,
+        "Invalid or missing input field",
+        validatedData.error
+      );
+    }
+    const { email, password } = validatedData.data;
+    // get user data
+    const getUser = await UserModel.findOne({ email });
+    if (!getUser) {
+      return response(false, 400, "Invalid login credentials.");
+    }
+
+    // resend email verification link
+
+    if (!getUser.isEmailVerified) {
+      const secret = new TextEncoder().encode(process.env.SECRET_KEY);
+      const token = await new SignJWT({ userId: getUser._id.toString() })
+        .setIssuedAt()
+        .setExpirationTime("1hr")
+        .setProtectedHeader({ alg: "HS256" })
+        .sign(secret);
+
+      await sendMail(
+        "Email Verification request from Admin",
+        email,
+        emailVerificationLink(
+          `${process.env.NEXT_PUBLIC_BASE_URL}/auth/verify-email/${token}`
+        )
+      );
+
+      return response(
+        false,
+        401,
+        ":Your Email is not verified yet. We have sent a verification link to your registered email address."
+      );
+    }
+    // password verification
+    const isPasswordVerified = await getUser.comparePassword(password);
+    if (!isPasswordVerified) {
+      return response(false, 400, "Invalid login credentials");
+    }
+
+    // otp generation
+
+    await OTPModel.deleteMany({ email }); //deleting old otps
+
+    const otp = generateOTP();
+
+    // storing otp into database
+
+    const newOtpData = new OTPModel({
+      email,
+      otp,
+    });
+
+    await newOtpData.save();
+
+    const otpEmailStatus = await sendMail(
+      "Your login Verification code",
+      email,
+      otpEmail(otp)
+    );
+    if (!otpEmailStatus.success) {
+      return response(false, 400, "Failed to send OTP.");
+    }
+    return response(true, 200, "Please Verify Your Device");
+  } catch (error) {
+    return catchError(error);
+  }
+}

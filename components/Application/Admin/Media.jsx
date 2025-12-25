abc2eccc42fdd525c1a2685addb02e8e8c7c7a27
mahidulkabir <mahidulkabir8@gmail.com>
2025-12-25 22:52:31 +0600

    added menu button for media page

---
+++ components/Application/Admin/Media.jsx
@@ -0,0 +1,84 @@
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ADMIN_MEDIA_EDIT } from "@/routes/AdminPanelRoute";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { MdOutlineModeEdit } from "react-icons/md";
+import { MdDeleteOutline } from "react-icons/md";
+import { IoLinkOutline } from "react-icons/io5";
+
+const Media = ({
+  media,
+  handleDelete,
+  deleteType,
+  selectedMedia,
+  setSelectedMedia,
+}) => {
+  const handleCheck = () => {};
+
+  return (
+    <div className="border border-gray-200 dark:border-gray-800 relative group rounded overflow-hidden">
+      <div className="absolute top-2 left-2 z-20">
+        <Checkbox
+          checked={selectedMedia.includes(media._id)}
+          onCheckedChange={handleCheck}
+          className={"border-primary"}
+        />
+      </div>
+      <div className="absolute top-2 right-2 z-20">
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <span className="w-7 h-7 flex items-center justify-center rounded-full bg-black/20 cursor-pointer">
+              <BsThreeDotsVertical color="#fff" />
+            </span>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="start">
+            {deleteType === "SD" && (
+              <>
+                <DropdownMenuItem className="cursor-pointer">
+                  <Link
+                    href={ADMIN_MEDIA_EDIT(media._id)}
+                    className="flex gap-1 justify-center items-center "
+                  >
+                    <MdOutlineModeEdit />
+                    Edit
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem className="cursor-pointer">
+                  <IoLinkOutline />
+                  Copy Link
+                </DropdownMenuItem>
+              </>
+            )}
+            <DropdownMenuItem className="cursor-pointer">
+                  <MdDeleteOutline color="red" />
+                    
+                    {deleteType === 'SD' ? 'Move Into Trash' : 'Delete Permanently'}
+                  
+                </DropdownMenuItem>
+
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="w-full h-full absolute z-10 transition-all duration-150 ease-in group-hover:bg-black/20"></div>
+      <div>
+        <Image
+          src={media?.secure_url}
+          alt={media?.alt || "Image"}
+          height={300}
+          width={300}
+          className="object-cover w-full sm:h-[200px] h-[150px]"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Media;
--- models/Media.model.js
+++ models/Media.model.js
@@ -15,6 +15,11 @@ const mediaSchema = new mongoose.Schema({
         required: true,
         trim:true
     },
+    secure_url:{
+        type:String,
+        required: true,
+        trim:true
+    },  
     alt:{
         type:String,
         trim:true

1385e0f1927235d6b547768ae6acfba5fce6eab9
mahidulkabir <mahidulkabir8@gmail.com>
2026-01-03 23:26:58 +0600

    added files for product 6:32

---
+++ app/(root)/(admin)/admin/product/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowProduct = () => {
+  return (
+    <div>
+      all product
+    </div>
+  )
+}
+
+export default ShowProduct
--- routes/AdminPanelRoute.js
+++ routes/AdminPanelRoute.js
@@ -10,6 +10,13 @@ export const ADMIN_MEDIA_EDIT   = (id) => id ? `/admin/media/edit/${id}` : ''
 export const ADMIN_CATEGORY_ADD   = '/admin/category/add'
 export const ADMIN_CATEGORY_SHOW   = '/admin/category'
 export const ADMIN_CATEGORY_EDIT  = (id)=>id ? `/admin/category/edit/${id}` : ''
+// Product routes 
+
+export const ADMIN_PRODUCT_ADD   = '/admin/product/add'
+export const ADMIN_PRODUCT_SHOW   = '/admin/product'
+export const ADMIN_PRODUCT_EDIT  = (id)=>id ? `/admin/product/edit/${id}` : ''
+
+
 // Trash Route 
 
 export const ADMIN_TRASH = '/admin/trash'

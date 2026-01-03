13ab32fe12eadc0af6144bb47387aa63e4d1840e
mahidulkabir <mahidulkabir8@gmail.com>
2026-01-03 13:03:44 +0600

    added route 5:58

--- app/(root)/(admin)/admin/category/edit/[id]/page.jsx
+++ app/(root)/(admin)/admin/category/edit/[id]/page.jsx
@@ -1,9 +1,134 @@
-import React from 'react'
+'use client'
+import BreadCrumb from '@/components/Application/Admin/BreadCrumb';
+import { ADMIN_CATEGORY_SHOW, ADMIN_DASHBOARD } from '@/routes/AdminPanelRoute';
+import React, { use, useEffect, useState } from 'react'
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import ButtonLoading from '@/components/Application/ButtonLoading';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { StrongAuthSchema } from '@/lib/zodSchema';
+import { useForm } from 'react-hook-form';
+import slugify from 'slugify';
+import { showToast } from '@/lib/showToast';
+import axios from 'axios';
+import useFetch from '@/hooks/useFetch';
+const breadCrumbData = [
+  { href: ADMIN_DASHBOARD, label: "Home" },
+  { href: ADMIN_CATEGORY_SHOW, label: "Category" },
+  { href: "", label: "Edit Category" },
+]
 
-const EditCategory = () => {
+
+const EditCategory = ({params}) => {
+  const {id} = use(params)
+  const {data: categorData} = useFetch(`/api/category/get/${id}`)
+  const [loading, setLoading] = useState(false);
+  const formSchema = StrongAuthSchema.pick({
+    name: true,
+    slug: true,
+  });
+
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: "",
+      slug: "",
+    },
+  });
+
+
+  useEffect(()=>{
+    const name = form.getValues('name')
+    if(name){
+      form.setValue('slug',slugify(name).toLowerCase())
+    }
+  },[form.watch('name')])
+
+  const onSubmit = async (values) =>{
+    setLoading(true)
+    try {
+        const {data:response} = await axios.post('/api/category/create', values)
+        if(!response.success){
+            throw new Error (response.message)
+        }
+
+        form.reset()
+        showToast('success', response.message)
+    } catch (error) {
+        showToast('error', error.message)
+    }finally{
+            setLoading(false)
+    }
+  }
+
+
+
   return (
     <div>
-      
+      <BreadCrumb breadCrumbData={breadCrumbData} />
+      <Card className="py-0 rounded shadow-sm">
+        <CardHeader className="pt-3 px-3 border-b [.border-b:pb-2] ">
+          <h4 className="font-semibold text-xl">Edit Category</h4>
+        </CardHeader>
+        <CardContent className="pb-5">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              
+              <div className="my-3">
+                <FormField
+                  control={form.control}
+                  name="name"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Name</FormLabel>
+                      <FormControl>
+                        <Input type="text" placeholder="Enter Category Name" {...field} />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+              <div className="my-3">
+                <FormField
+                  control={form.control}
+                  name="slug"
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel>Slug</FormLabel>
+                      <FormControl>
+                        <Input
+                          type="text"
+                          placeholder="Enter Slug"
+                          {...field}
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+              </div>
+
+              <div>
+                <ButtonLoading
+                  loading={loading}
+                  type="submit"
+                  text="Add Category"
+                  className="cursor-pointer"
+                />
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   )
 }
